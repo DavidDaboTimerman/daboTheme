1f95fe15772b79d868655081e07654692ad1510d
david.timerman@sergata.com <david.timerman@sergata.com>
2015-07-04 11:07:23 -0500

    added init file , header php ,roadmap.md and new code in functions and index page

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if !IE]><!--> <html <?php language_attributes(); ?>> <!--<![endif]-->
+
+<head>
+	<meta charset="<?php bloginfo('charset');?>">
+	<title>David Timerman Theme</title>
+	<meta name="description" content="This is a theme created by the world's greatest Web Developer">
+	<meta name="author" content="David Timerman">
+	<!-- Mobile Specific Data -->
+	<meta name="viewport" content="width-device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- Stylesheets -->
+	<link rel="Stylesheet" href="style.css" />
+	<link href="http://fonts.googleapis.com/css?family=Bitter:400,700" rel="stylesheet">
+	<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,600,300italic.600italic" rel="stylesheet">
+
+	<!--[if lt IE 9]>
+		<script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]--> 
+</head>
+<body>
+
+	<!-- HEADER -->
+	<header class="main-header section-content align-center">
+		
+	</header>
--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php ?>
+<?php 
+/** 
+ *
+ *	Index.php
+ * 
+ *	The Main Template File
+ *
+ * 
+ */
+
+?>
+
+<?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
+<?php endwhile; ?>
